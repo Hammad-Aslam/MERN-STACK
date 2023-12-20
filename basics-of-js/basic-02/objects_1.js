6fa53970e2da10a82351d03b6f0dab7736daf45b
Hammad <71564810+Hammad-Aslam@users.noreply.github.com>
2023-12-20 08:40:15 +0000

    Add objects part 1

--- basics-of-js/basic-02/objects_1.js
+++ basics-of-js/basic-02/objects_1.js
@@ -7,12 +7,12 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "HAMMAD",
+    "full name": "HAMMAD ASLAM",
     [mySym]: "mykey1",
     age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    location: "KARACHI",
+    email: "hammad@gmail.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -22,9 +22,9 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "hammad@yahoo.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "hammad@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
